32ec1d586580bb3e6af41960e489bfd79f2473f1
Jinn42 <58035382+Jinn42@users.noreply.github.com>
2020-05-11 12:58:58 +0200

    Create reducer.py

---
+++ MapReduce/most_frequent_word/reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from operator import itemgetter
+import sys
+
+max = 0
+maxword = ""
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    
+    key, word, count = line.split('\t', 2)
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        continue
+    
+    if count > max:
+        max = count
+        maxword=word
+    
+print 'most frequent word:\t-%s-\t%s times' % (maxword, max)
